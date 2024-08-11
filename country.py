5466f6c12156970196314ad9e04ac7b89c5e71cc
Juan Camilo Grisales <131409966+Grisales1803@users.noreply.github.com>
2024-08-11 15:00:54 -0600

    Add files via upload

--- country.py
+++ country.py
@@ -14,16 +14,19 @@ def info_country(country):
       # Check if the response is a list and contains at least one element
       if isinstance(response_dict, list) and len(response_dict) > 0:
           info_country = response_dict[0]
+          name = info_country['name']['common']
           flag = info_country['flags']['png']
           flag_meaning = info_country['flags']['alt']
       else:
           # Handle cases where the response is not as expected
           flag = "https://static.vecteezy.com/system/resources/previews/028/086/717/original/ocean-water-surface-waves-isolated-on-transparent-background-file-cut-out-ai-generated-png.png"
           flag_meaning = "The ISS is over water"
+          name = "The Ocean"
   except Exception:
       # Handle any exceptions that occur during the API call or JSON parsing
       flag = "https://via.placeholder.com/150?text=Error"
       flag_meaning = "Error retrieving country information"
+      name = "Error"
 
   # Return the flag and flag meaning
-  return flag, flag_meaning
+  return flag, flag_meaning, name
--- main.py
+++ main.py
@@ -20,16 +20,16 @@ def home():
   # Obtain the flag and the flag meaning of the country
   country = country_code
   if water == False:
-    flag, flag_meaning = info_country(country)
+    flag, flag_meaning, country_name = info_country(country)
   else:
     flag = "https://static.vecteezy.com/system/resources/previews/028/086/717/original/ocean-water-surface-waves-isolated-on-transparent-background-file-cut-out-ai-generated-png.png"
     flag_meaning = "The ISS is over water"
-    country = "The Ocean"
+    country_name = "The Ocean"
   # Obtain the distance from the ISS to my location, which is Saskatoon, SK, Canada
   lat2, lon2 = 52.133220, -106.670070 # Saskatoon, SK coordinates
   dist = distance_to_ISS(lat, lon, lat2, lon2)
   # Return the values that are going to be used in the HTML page
-  return render_template("index.html", lat=lat, lon=lon, map=map, temp=temp, description=description,country=country, flag=flag, flag_meaning=flag_meaning, dist=dist)
+  return render_template("index.html", lat=lat, lon=lon, map=map, temp=temp, description=description,country=country_name, flag=flag, flag_meaning=flag_meaning, dist=dist)
 
 # Run the app
 app.run(host='0.0.0.0', port=8080)
